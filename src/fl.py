298cb308e078594fe3548a14a9bf1550d4fba189
AHAMED THAIYUB A <98688617+Ahamedthaiyub@users.noreply.github.com>
2023-09-02 22:56:52 +0530

    Add files via upload

---
+++ src/fl.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+models = {
+    'model1': {
+        'name': 'Model 1',
+        'status': 'Idle',
+    },
+    'model2': {
+        'name': 'Model 2',
+        'status': 'Idle',
+    }
+}
+
+@app.route('/train/Model 1', methods=['POST'])
+def train_model(model_name):
+    if model_name in models:
+        
+        models[model_name]['status'] = 'Training'
+        return jsonify({'message': f'Training {models[model_name]["name"]}'}), 202
+    else:
+        return jsonify({'error': 'Model not found'}), 404
+
+@app.route('/predict/<model_name>', methods=['POST'])
+def predict(model_name):
+    if model_name in models:
+        
+        models[model_name]['status'] = 'Predicting'
+        return jsonify({'message': f'Predicting using {models[model_name]["name"]}...'}), 202
+    else:
+        return jsonify({'error': 'Model not found'}), 404
+
+@app.route('/models', methods=['GET'])
+def get_models():
+    return jsonify({'models': models}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
